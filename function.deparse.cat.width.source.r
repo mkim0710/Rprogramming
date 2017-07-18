9e9195ce45c05dfa2d5c174882ef014881141587
mkim0710 <mkim0710@users.noreply.github.com>
2017-07-18 17:56:49 -0400

    Update function.deparse.cat.width.source.r

--- function.deparse.cat.width.source.r
+++ function.deparse.cat.width.source.r
@@ -3,9 +3,11 @@ function.deparse.cat.width = function(x, width.cutoff = 500) {
   cat(deparse(x, width.cutoff = width.cutoff), sep = "\n")
 }
 
-# > dput(1:30*2)
-# c(2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 
-# 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60)
-# > function.deparse.cat.width(1:30*2)
-# c(2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60)
+#@ test) function.deparse.cat.width() --------
+# > dput(letters)
+# c("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", 
+# "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", 
+# "z")
+# > function.deparse.cat.width(letters)
+# c("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
 
