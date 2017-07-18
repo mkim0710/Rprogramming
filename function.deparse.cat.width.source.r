71d9947a6536c0d555127127ec75abb234419be3
mkim0710 <mkim0710@users.noreply.github.com>
2017-07-18 01:49:20 -0400

    Create function.deparse.cat.width.source.r

---
+++ function.deparse.cat.width.source.r
@@ -0,0 +1,10 @@
+function.deparse.cat.width = function(x, width.cutoff=200) {
+  cat(deparse(x, width.cutoff = width.cutoff), sep = "\n")
+}
+
+# > dput(1:30*2)
+# c(2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 
+# 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60)
+# > function.deparse.cat.width(1:30*2)
+# c(2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60)
+
